970d20d1e2d5a57955a1ffec29c945e737d8a759
Huigesi <791339970@qq.com>
2018-03-11 11:34:38 +0800

    4-2 MainActivity

--- app/src/main/java/cn/edu/gdmec/android/boxuegutestdemo/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegutestdemo/MainActivity.java
@@ -1,14 +1,46 @@
 package cn.edu.gdmec.android.boxuegutestdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView tv_back;
+    private TextView tv_main_title;
+    private RelativeLayout title_bar;
+    private FrameLayout main_body;
+    private TextView bottom_bar_text_course;
+    private ImageView bottom_bar_image_course;
+    private TextView bottom_bar_text_exercises;
+    private ImageView bottom_bar_image_exercises;
+    private TextView bottom_bar_text_myinfo;
+    private ImageView bottom_bar_image_myinfo;
+    private LinearLayout main_bottom_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv_back = (TextView) findViewById(R.id.tv_back);
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        main_body = (FrameLayout) findViewById(R.id.main_body);
+        bottom_bar_text_course = (TextView) findViewById(R.id.bottom_bar_text_course);
+        bottom_bar_image_course = (ImageView) findViewById(R.id.bottom_bar_image_course);
+        bottom_bar_text_exercises = (TextView) findViewById(R.id.bottom_bar_text_exercises);
+        bottom_bar_image_exercises = (ImageView) findViewById(R.id.bottom_bar_image_exercises);
+        bottom_bar_text_myinfo = (TextView) findViewById(R.id.bottom_bar_text_myinfo);
+        bottom_bar_image_myinfo = (ImageView) findViewById(R.id.bottom_bar_image_myinfo);
+        main_bottom_bar = (LinearLayout) findViewById(R.id.main_bottom_bar);
     }
 }
