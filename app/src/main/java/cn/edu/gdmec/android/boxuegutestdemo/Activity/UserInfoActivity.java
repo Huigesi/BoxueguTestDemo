17b70050013b9c813d55a08d34839444936e169e
Huigesi <791339970@qq.com>
2018-03-12 09:29:45 +0800

    5.1 UserInfoActivity

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegutestdemo/Activity/UserInfoActivity.java
@@ -0,0 +1,137 @@
+package cn.edu.gdmec.android.boxuegutestdemo.Activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cn.edu.gdmec.android.boxuegutestdemo.Bean.UserBean;
+import cn.edu.gdmec.android.boxuegutestdemo.R;
+import cn.edu.gdmec.android.boxuegutestdemo.Utils.AnalysisUtils;
+import cn.edu.gdmec.android.boxuegutestdemo.Utils.DBUtils;
+
+public class UserInfoActivity extends Activity implements View.OnClickListener{
+
+    private TextView tv_back;
+    private TextView tv_main_title;
+    private RelativeLayout title_bar;
+    private ImageView iv_head_icon;
+    private TextView tv_user_name;
+    private TextView tv_nickName;
+    private TextView tv_sex;
+    private TextView tv_signature;
+    private RelativeLayout rl_head;
+    private RelativeLayout rl_account;
+    private RelativeLayout rl_nickName;
+    private RelativeLayout rl_sex;
+    private RelativeLayout rl_signature;
+    private String spUserName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+        spUserName= AnalysisUtils.readLoginUserName(this);
+        initView();
+        initData();
+    }
+
+
+    private void initData() {
+        UserBean bean=null;
+        bean= DBUtils.getInstance(this).getUserInfo(spUserName);
+        if (bean==null){
+            bean=new UserBean();
+            bean.userName=spUserName;
+            bean.nickName="问答精灵";
+            bean.sex="男";
+            bean.signature="问答精灵";
+            DBUtils.getInstance(this).saveUserInfo(bean);
+        }
+        satValue(bean);
+    }
+
+    private void satValue(UserBean bean) {
+        tv_user_name.setText(bean.userName);
+        tv_nickName.setText(bean.nickName);
+        tv_sex.setText(bean.sex);
+        tv_signature.setText(bean.signature);
+    }
+
+    private void initView() {
+        tv_back = (TextView) findViewById(R.id.tv_back);
+        tv_back.setOnClickListener(this);
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("个人资料");
+        title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
+        iv_head_icon = (ImageView) findViewById(R.id.iv_head_icon);
+        tv_user_name = (TextView) findViewById(R.id.tv_user_name);
+        tv_nickName = (TextView) findViewById(R.id.tv_nickName);
+        tv_sex = (TextView) findViewById(R.id.tv_sex);
+        tv_signature = (TextView) findViewById(R.id.tv_signature);
+        rl_head = (RelativeLayout) findViewById(R.id.rl_head);
+        rl_head.setOnClickListener(this);
+        rl_account = (RelativeLayout) findViewById(R.id.rl_account);
+        rl_account.setOnClickListener(this);
+        rl_nickName = (RelativeLayout) findViewById(R.id.rl_nickName);
+        rl_nickName.setOnClickListener(this);
+        rl_sex = (RelativeLayout) findViewById(R.id.rl_sex);
+        rl_sex.setOnClickListener(this);
+        rl_signature = (RelativeLayout) findViewById(R.id.rl_signature);
+        rl_signature.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_back:
+                this.finish();
+                break;
+            case R.id.rl_nickName:
+                //昵称
+                break;
+            case R.id.rl_sex:
+                String sex=tv_sex.getText().toString();
+                sexDiolag(sex);
+
+                break;
+            case R.id.rl_signature:
+                //signature
+                break;
+        }
+    }
+
+    private void sexDiolag(String sex) {
+        int sexFlag=0;
+        if ("男".equals(sex)){
+            sexFlag=0;
+        }else if ("女".equals(sex)){
+            sexFlag=1;
+        }
+        final String item[]={"男","女"};
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("性别");
+        builder.setSingleChoiceItems(item, sexFlag, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(UserInfoActivity.this,item[which],Toast.LENGTH_SHORT).show();
+                setSex(item[which]);
+            }
+        });
+        builder.create().show();
+
+    }
+
+    private void setSex(String s) {
+        tv_sex.setText(s);
+        DBUtils.getInstance(UserInfoActivity.this).updateUserInfo("sex",s,spUserName);
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegutestdemo/Bean/UserBean.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.boxuegutestdemo.Bean;
+
+/**
+ * Created by Administrator on 2018/3/12.
+ */
+
+public class UserBean {
+    public String userName;
+    public String nickName;
+    public String sex;
+    public String signature;
+
+}
